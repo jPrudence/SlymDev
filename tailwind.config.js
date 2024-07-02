760bda7cbcfca28ae870f9a34c29782b1909ce6e
JPrudence <rjeanprudence@gmail.com>
2024-07-02 02:02:03 +0200

    Integrate custom styles for Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,35 @@ export default {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                secondary: "var(--color-secondary)",
+                "gradient-primary": "var(--color-gradient-primary)",
+                grey: {
+                    900: "#25213E",
+                    800: "#514f5f",
+                    600: "#7E7D84",
+                    400: "#a7a7a5",
+                },
+            },
+            boxShadow: {
+                card: "0px 0px 24px rgba(0, 0, 0, 0.03)",
+            },
+            width: {
+                30: "7.5rem",
+                95: "23.75rem",
+            },
+            fontSize: {
+                tiny: "0.625rem",
+            },
+            lineHeight: {
+                4.5: "1.125rem",
+            },
+            letterSpacing: {
+                1: "0.063rem",
+                0.01: "0.01rem",
+            },
+        },
     },
     plugins: [],
-}
+};
